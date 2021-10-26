ce542bfa65e42a7693f0fb6ab12694829cba433e
yanwen <yanwenci@outlook.com>
2021-10-26 07:05:56 +0100

    t

--- voxelmorph/tf/networks.py
+++ voxelmorph/tf/networks.py
@@ -1340,7 +1340,7 @@ class HyperUnetDense(ne.modelio.LoadableModel):
         # outputs =  tf.concat((seg1,seg2,seg3),-1)
 
         super().__init__(name=name, inputs=[source1, source2, source3, hyp_input], outputs=[
-                                                                                            #unet_model1.output,unet_model2.output,unet_model3.output,
+                                                                                            unet_model1.output,unet_model2.output,unet_model3.output,
                                                                                             outputs])
 
         # cache pointers to layers and tensors for future reference
