6a2f14569ecdaebc5078be2556c2f6b7d4246c35
yanwenCi <yanwenCi@outlook.com>
2022-01-14 14:22:37 +0800

    first commit

--- hyperp_generator.py
+++ hyperp_generator.py
@@ -39,29 +39,29 @@ class HyperLinear(nn.Module):
 class hyperp_generator():
     def __init__(self, fix_hyp):
 
-        # self.X = torch.tensor([
-        # [1.0, 0.0, 0.0],
-        # [1.0, 0.0, 1.0],
-        # [1.0, 1.0, 0.0],
-        # [1.0, 1.0, 1.0],
-        # [0.0, 0.0, 0.0],
-        # [0.0, 0.0, 1.0],
-        # [0.0, 1.0, 0.0],
-        # [0.0, 1.0, 1.0]
-        # ]).cuda()
-        self.X = torch.tensor(np.random.uniform(low=-20, high=20, size=(8,3))).float().cuda()
+        self.X = torch.tensor([
+        [1.0, 0.0, 0.0],
+        [1.0, 0.0, 1.0],
+        [1.0, 1.0, 0.0],
+        [1.0, 1.0, 1.0],
+        [0.0, 0.0, 0.0],
+        [0.0, 0.0, 1.0],
+        [0.0, 1.0, 0.0],
+        [0.0, 1.0, 1.0]
+        ]).cuda()
+        #self.X = torch.tensor(np.random.uniform(low=-20, high=20, size=(8,3))).float().cuda()
         #for verify
 
         #p=torch.Tensor([-0.3570, 19.9367, -0.3570, -9.4176]).reshape(-1,1)
 
     # output - 0 and 1 being negative and positive, respectively
-        self.y=torch.FloatTensor([np.random.choice(2,size=(8))]).permute(1,0).cuda()
-        self.y[3]=1
+
+        #self.y=torch.FloatTensor([np.random.choice(2,size=(8))]).permute(1,0).cuda()
+        #3self.y[3]=1
 
         #self.y = torch.tensor([1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0]).unsqueeze(1).cuda() # to test invariance to 3rd var X[,2]
-
-        self.hyp=fix_hyp[0]*1.0
-
+        self.y = torch.FloatTensor([int(h) for h in '{0:08b}'.format(fix_hyp)]).unsqueeze(1).cuda()
+        #self.hyp=fix_hyp[0]*1.0
         #self.model = torch.nn.Sequential(HyperLinear(3,1,use_bias=True, bias_value=self.hyp),  torch.nn.Sigmoid()).cuda()
         self.model = torch.nn.Sequential(torch.nn.Linear(3, 1, bias=True), torch.nn.Sigmoid()).cuda()
         self.cross_entropy = torch.nn.BCELoss()
@@ -75,15 +75,19 @@ class hyperp_generator():
             self.optimiser.step()
         #a_lreg = torch.cat((self.model[0].weight.data.squeeze(), self.hyp.cuda()), dim=0)
         a_lreg = torch.cat((self.model[0].weight.data.squeeze(), self.model[0].bias.data), dim=0)
-        p=a_lreg.reshape(-1,1)
-        x=torch.cat((self.X, torch.ones(8,1).cuda()),1)
-        y=torch.mm(x,p)
-        y=torch.round(torch.sigmoid(y))
-        yy=torch.round(self.model(self.X))
+
+        # # manual validation
+        #p=a_lreg.reshape(-1,1)
+        # x=torch.cat((self.X, torch.ones(8,1).cuda()),1)
+        # y=torch.mm(x,p)
+        # y=torch.round(torch.sigmoid(y))
+        # # convolution validation
+        # yy=torch.round(self.model(self.X))
         #print(y.squeeze(), self.y.squeeze(), yy.squeeze())
         res_lreg = pred - self.y
 
         return a_lreg, res_lreg, self.y.data.cpu().numpy()
+
 
     #a_lreg = torch.cat([model[0].weight.data.squeeze(), model[0].bias.data],0)
     def generator(self):
@@ -94,22 +98,52 @@ class hyperp_generator():
             return a_lreg, y
 
 
+def valid():
+    space=[]
+    hyp=np.load('hyperp.npy')
+    X = torch.tensor([
+        [1.0, 0.0, 0.0],
+        [1.0, 0.0, 1.0],
+        [1.0, 1.0, 0.0],
+        [1.0, 1.0, 1.0],
+        [0.0, 0.0, 0.0],
+        [0.0, 0.0, 1.0],
+        [0.0, 1.0, 0.0],
+        [0.0, 1.0, 1.0]
+    ])
+    X1=torch.cat([X, torch.ones(8, 1)], dim=1)
+    for i in range(len(hyp)):
+        yy = torch.round(torch.sigmoid(X1 @ hyp[i]))
+        space.append(yy.numpy())
+    with open('sample_Y.csv', 'w') as file:
+        writer = csv.writer(file)
+        writer.writerows(space)
+    np.save('sample_Y.npy', np.array(space))
 
 if __name__=='__main__':
     import os
+    import csv
     os.environ["CUDA_VISIBLE_DEVICES"]='2'
     space=[]
+    Y=[]
     i=0
-    a=np.load('hyperp.npy')
-    while True:
-        if i>200:
-            break
-        else:
-
-            hyp_g=hyperp_generator(torch.tensor([[i]]))
-            if hyp_g.generator() is not None:
-                i+=1
-                space.append(np.array(hyp_g.generator()[0].cpu().data))
-                print (hyp_g.generator())
+    # while True:
+    #     if i>200:
+    #         break
+    #     else:
+    #
+    #         hyp_g=hyperp_generator(torch.tensor([[i]]))
+    #         if hyp_g.generator() is not None:
+    #             i+=1
+    #             space.append(np.array(hyp_g.generator()[0].cpu().data))
+    #             print (hyp_g.generator())
             #np.savetxt('hyper.txt', hyp_g.generator()[1].cpu().data)
-    np.save('hyperp_range20.npy', np.array(space))
+    for i in range(1, np.power(2,8)):
+            hyp_g = hyperp_generator(i)
+            if hyp_g.generator() is not None:
+                #i += 1
+                space.append(np.array(hyp_g.generator()[0].cpu().data))
+                print(hyp_g.generator())
+                Y.append(np.array([int(h) for h in '{0:08b}'.format(i)]))
+    np.save('hyperp255.npy', np.array(space))
+    np.save('sample_Y255.npy', Y)
