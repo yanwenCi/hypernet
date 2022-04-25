226c47ee4b8b59de2c6e9e7053aae78e7f5a8d0c
yanwenCi <yanwenCi@outlook.com>
2022-04-25 10:24:30 -0200

    241

--- get_test.py
+++ get_test.py
@@ -3,17 +3,23 @@ import numpy as np
 data=np.load('hyperp_test.npy')
 print(data.shape)
 f=open('test_hyper2.sh','w')
+decision=np.load('sample_test.npy')
 f.write('#!/bin/bash\n')
 cmd=[]
-for j in range(len(data)):
-    i=np.random.randint(len(data))
+for i in range(len(data)):
+    #i=np.random.randint(len(data))
     a,b,c,d=data[i]
+    y=decision[i].tolist()
+    y=y[4:]+y[0:4]
+    no=[str(p) for p in y]
+    no=int(''.join(no),2)
+    
     if d>0:
         continue
     else:
         print(i, np.round(a,4))
         #cmd.append('python train_hypermorph.py --img-list data/data_lesion_cross1/ --model-dir checkpoints/combiner2_cross1/{}  --mod 2  --gpu 2 --epoch 20  --batch-size 4  --steps-per-epoch 300  --hyper-val ,{:>6f},{:>6f},{:>6f},{:>6f}\n'.format(i,a,b,c,d))
-        cmd.append('python test_hyper.py --img-list data/data_lesion_cross1/ --model-dir checkpoints/hyper2_cross1  --mod 2 --load-weights 143  --gpu 2 --hyper_val ,{:>6f},{:>6f},{:>6f},{:>6f}   --pred-dir Pred_dir/hyper2_cross1/{} >checkpoints/hyper2_cross1/test{}_log\n'.format(a,b,c,d,i,i))
+        cmd.append('python test_hyper.py --img-list data/data_lesion_cross1/ --model-dir checkpoints/hyper2_cross1  --mod 2 --load-weights 143  --gpu 2 --hyper_val ,{:>6f},{:>6f},{:>6f},{:>6f}   --pred-dir Pred_dir/hyper2_cross1/{} >checkpoints/hyper2_cross1/test{}_log\n'.format(a,b,c,d,no,no))
 
 f.writelines(cmd)
 f.close()
--- get_test_combiner.py
+++ get_test_combiner.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 
+decision=np.load('sample_test.npy')
 data=np.load('hyperp_test.npy')
 print(data.shape)
 f=open('test_combiner2.sh','w')
@@ -8,6 +9,11 @@ f.write('#!/bin/bash\n')
 cmd=[]
 for i in range(len(data)):
     #i=np.random.randint(len(data))
+    y=decision[i]
+    y=Y[i].tolist()
+    y=y[4:]+y[0:4]
+    no=[str(p) for p in y]
+    no=int(''.join(no),2)
     a,b,c,d=data[i]
     if d>0:
         continue
@@ -22,7 +28,7 @@ for i in range(len(data)):
         e=max(files)
         print(e)
         #cmd.append('python train_nohyper.py --img-list data/data_lesion_cross1/ --lr 1e-4  --model-dir checkpoints/combiner2_cross1/{}  --mod 2  --gpu 1 --epoch 20 --patience 3  --batch-size 4 --steps-per-epoch 300  --hyper-val ,{:>6f},{:>6f},{:>6f},{:>6f}\n'.format(i,a,b,c,d))
-        cmd.append('python test_hyper.py --img-list data/data_lesion_cross1/ --model-dir checkpoints/combiner2_cross1/{}  --mod 2 --load-weights {}  --net combiner  --gpu 2 --hyper_val ,{:>6f},{:>6f},{:>6f},{:>6f}   --pred-dir Pred_dir/combiner2_cross1/{} >checkpoints/combiner2_cross1/{}/test_log\n'.format(i,e,a,b,c,d,i,i))
+        cmd.append('python test_hyper.py --img-list data/data_lesion_cross1/ --model-dir checkpoints/combiner2_cross1/{}  --mod 2 --load-weights {}  --net combiner  --gpu 2 --hyper_val ,{:>6f},{:>6f},{:>6f},{:>6f}   --pred-dir Pred_dir/combiner2_cross1/{} >checkpoints/combiner2_cross1/{}/test_log\n'.format(no,e,a,b,c,d,no,no))
 
 f.writelines(cmd)
 f.close()
