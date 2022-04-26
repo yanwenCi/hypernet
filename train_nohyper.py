8330fd3142248acdcb4dc068b164377623d97631
yanwen <yanwenci@outlook.com>
2022-04-26 11:16:22 +0100

    132

--- train_nohyper.py
+++ train_nohyper.py
@@ -226,7 +226,7 @@ with tf.device(device):
     # prepare loss functions and compile model
 
     if args.mod==0:
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=args.lr), loss=[image_loss_func
+        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=args.lr), loss=[image_loss_func
         ,image_loss1, image_loss2, image_loss3,
                                                                                    ])
     elif args.mod==2:
