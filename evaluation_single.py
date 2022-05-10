2b5d30f23ef2ac4d4f740919fcba8e5928733ba5
yanwen <yanwenci@outlook.com>
2022-05-10 09:56:46 +0100

    132

--- evaluation_single.py
+++ evaluation_single.py
@@ -38,28 +38,19 @@ def main(args):
             # dwi = nib.load(path_dwi).get_fdata()
             lab = nib.load(path_lab).get_fdata()
             pre = nib.load(path_pre).get_fdata()
-            if args.zone=='pz':
-                zone=nib.load(path_pz).get_fdata()
-                lab=zone*lab
-                pre=zone*pre
-            elif args.zone=='tz':
-                zone = nib.load(path_tz).get_fdata()    
-                lab=zone*lab
-                pre=zone*pre
-            hausd_dist, dice_vals, precision, recal,number_tplesion_gt, number_tplesion_pd, od_acc_tp, od_acc_fp, gt_number, pred_number=metrics(pre, lab)
+            hausd_dist, dice_vals,number_tplesion_gt, number_tplesion_pd, gt_number, pred_number=metrics(pre, lab)
             dice_mean.append([path_pre.split('/')[-1]]+hausd_dist.tolist() + dice_vals.tolist() + precision.tolist() + recal.tolist())
             number_of_pg.append([path_pre.split('/')[-1]]+number_tplesion_gt.tolist() + number_tplesion_pd.tolist())
-            number_of_od.append([path_pre.split('/')[-1]]+od_acc_tp.tolist()+ od_acc_fp.tolist())
+            #number_of_od.append([path_pre.split('/')[-1]]+od_acc_tp.tolist()+ od_acc_fp.tolist())
             number_of_pred.append([path_pre.split('/')[-1]]+pred_number.tolist()+[gt_number])
-            od_tp+=od_acc_tp
-            od_fp+=od_acc_fp
+            #od_tp+=od_acc_tp
+            #od_fp+=od_acc_fp
             pred_acc+=pred_number
             gt_acc_lesion+=gt_number
 
             gt_tp+=number_tplesion_gt
             pd_tp+=number_tplesion_pd
     save_path=join(path, 'metrics')
-    print(od_tp, od_fp, pred_acc)
     if not os.path.exists(save_path):
         os.makedirs(save_path)
         print(save_path)
@@ -67,9 +58,9 @@ def main(args):
         write = csv.writer(f_csv)
         write.writerows(dice_mean)
 
-    with open(join(save_path, 'object.csv'), 'w+') as f_csv:
-        write = csv.writer(f_csv)
-        write.writerows(number_of_od)
+    #with open(join(save_path, 'object.csv'), 'w+') as f_csv:
+    #    write = csv.writer(f_csv)
+    #    write.writerows(number_of_od)
     with open(join(save_path, 'PR.csv'), 'w+') as f_csv:
         write = csv.writer(f_csv)
         write.writerows(number_of_pg)
@@ -79,8 +70,8 @@ def main(args):
 
     f_csv.close()
     print(np.mean(np.array(dice_mean)[:,1:].astype(np.float),axis=0))
-    print('od', od_tp / (od_tp + od_fp), od_tp / gt_acc_lesion)
-    print(gt_acc_lesion)
+    #print('od', od_tp / (od_tp + od_fp), od_tp / gt_acc_lesion)
+    print(gt_acc_lesion,gt_acc_lesion)
     print('gtpd', gt_tp/gt_acc_lesion, pd_tp/pred_acc)
 
 def removesamll(contours, thres=0.5):
@@ -97,23 +88,35 @@ def removesamll(contours, thres=0.5):
     print('    Founding {} contours'.format(len(cv_contours)))
     return cv_contours
 
-def metrics(pred, target):
-    od_acc_tp=np.zeros(9)
-    od_acc_fp=np.zeros(9)
+def metrics(pred, target_, t_zone, p_zone):
+    iters=9*3
+    od_acc_tp=np.zeros(iters)
+    od_acc_fp=np.zeros(iters)
     od_gt_number=0
-    pred_number=np.zeros(9)
-    hausd_dist=np.zeros(9)
-    dice_vals=np.zeros(9)
-    precision=np.zeros(9)
-    recal=np.zeros(9)
-    number_tp_pd=np.zeros(9)
-    number_tp_gt=np.zeros(9)
-    pred_lesion=np.zeros(9)
-    gt_lesion=np.zeros(9)
+    pred_number=np.zeros(iters)
+    hausd_dist=np.zeros(iters)
+    dice_vals=np.zeros(iters)
+    precision=np.zeros(iters)
+    recal=np.zeros(iters)
+    number_tp_pd=np.zeros(iters)
+    number_tp_gt=np.zeros(iters)
+    pred_lesion=np.zeros(iters)
+    gt_lesion=np.zeros(iters)
     dice_score=vxm.losses.Dice(with_logits=False)
-    for p in range(1,10):
+    for p in range(1,28):
         pred_seg=copy.deepcopy(pred)
-        thre=p/10
+        target=copy.deepcopy(target_)
+        if p>9 and p<19:
+            thre=(p-9)/10
+            pred_seg=t_zone*pred_seg
+            target=t_zone*target
+        elif p>18:
+            thre=(p-18)/10
+            pred_seg=p_zone*pred_seg
+            target=p_zone*target
+        else:
+            thre=p/10
+        
         pred_seg[pred_seg>thre]=1
         pred_seg[pred_seg<=thre]=0
 
@@ -121,19 +124,19 @@ def metrics(pred, target):
         surf_dist=sd.compute_surface_distances(np.array(pred_seg, dtype=bool), np.array(target, dtype=bool), (1,1,1))
         hausd_dist[p-1]=sd.compute_robust_hausdorff(surf_dist,95)
         hausd_dist[np.isinf(hausd_dist)]=50
-        prec, rec = precision_and_recall(target, pred_seg,2)
-        precision[p-1]=prec[-1]
-        recal[p-1]=rec[-1]
+        #prec, rec = precision_and_recall(target, pred_seg,2)
+        #precision[p-1]=prec[-1]
+        #recal[p-1]=rec[-1]
 
         #calculate object detection metric
-        od_iou, number_lesion_tgt, number_lesion__pred, tp_num, fp_num=OD_PR(target, pred_seg, 0.25)
-        od_acc_tp[p-1]=tp_num
-        od_acc_fp[p-1]=fp_num
+        #od_iou, number_lesion_tgt, number_lesion__pred, tp_num, fp_num=OD_PR(target, pred_seg, 0.25)
+        #od_acc_tp[p-1]=tp_num
+        #od_acc_fp[p-1]=fp_num
         thre=[0.25]
         overlap_pd, number_tp_pd[p-1], pred_lesion[p-1]=pn_rate(target,  pred_seg, thre,direct='pred')
         overlap_gt, number_tp_gt[p-1], gt_lesion[p-1]=pn_rate(target,  pred_seg,thre, direct='gt')
-
-    return hausd_dist, dice_vals, precision, recal,number_tp_gt, number_tp_pd, od_acc_tp, od_acc_fp, number_lesion_tgt, pred_lesion
+    return hausd_dist, dice_vals,number_tp_gt, number_tp_pd, gt_lesion, pred_lesion
+    #return hausd_dist, dice_vals, precision, recal,number_tp_gt, number_tp_pd, od_acc_tp, od_acc_fp, number_lesion_tgt, pred_lesion
 
 
 if __name__=='__main__':
