558eeef9e0fa825afcddadf1a8d3026f50435aa3
yanwenCi <yanwenCi@outlook.com>
2022-05-26 07:30:29 -0200

    241

--- statistic_result.py
+++ statistic_result.py
@@ -32,12 +32,12 @@ for i in range(len(data)):
         continue
     log=open(path_log,'r')
     log_=log.readlines()
-    log_line=log_[-2].split(' ')
+    log_line=log_[1].split(' ')
     log_line=[i.replace('[','').strip(',') for i in log_line]
     log_line=list(filter(None, log_line))[1:7]
     log_line=[float(k.strip().strip('[').strip(']')) for k in log_line]
 #std   
-    log_std=log_[-3]
+    log_std=log_[0]
     log_std=log_std.split(' ')
     log_std=[i.replace('[','').strip(',') for i in log_std]
     log_std=list(filter(None, log_std))[1:7]
@@ -48,9 +48,23 @@ for i in range(len(data)):
     log_lesion=[i.replace('[','').strip(',') for i in log_lesion]
     log_lesion=list(filter(None, log_lesion))[1:7]
     log_lesion=[float(k.strip().strip('[').strip(']')) for k in log_lesion]
+#auc
+    log_auc=log_[3]
+    log_auc=log_auc.split(' ')
+    log_auc=[i.replace('[','').strip(',') for i in log_auc]
+    log_auc=list(filter(None,log_auc))[1:4]
+    log_auc=[float(k.strip().strip('[').strip(']')) for k in log_auc]
 
+#lesion0.5
+    log_lesion5=log_[2]
+    log_lesion5=log_lesion5.split(':')[-1].split(' ')
+    log_lesion5=[i.replace('[','').strip(',') for i in log_lesion5]
+    log_lesion5=list(filter(None, log_lesion5))[0:6]
+    log_lesion5=[float(k.strip().strip('[').strip(']')) for k in log_lesion5]
+
+    log_dice=['%.2f(%.2f)' % (k,l) for k, l in zip(log_line, log_std)]
     print([no]+y+hyp.tolist()+log_line+log_std+log_lesion)
-    new.append([no]+y+hyp.tolist()+log_line+log_std+log_lesion)
+    new.append([no]+y+hyp.tolist()+log_dice+log_lesion+log_auc+log_line+log_lesion5)
     
 
 with open(os.path.join('checkpoints',f,'results_log_{}.csv'.format(args.phase)),'w') as file:
